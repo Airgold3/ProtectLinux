29b9dad7dd85be568743cbcf2301064cc3b19057
Airgold3 <airgold3yt@gmail.com>
2024-04-18 10:05:46 +0200

    Update protectlinux.sh

--- protectlinux.sh
+++ protectlinux.sh
@@ -2,7 +2,7 @@
 # AUTHOR: Airgold3 & https://github.com/Airgold3
 # This file is part of Automatic Fivem Server Creator
 # Copyright (C) 2024, Airgold3
-#    Automatic Fivem Server Creator ( is free software: you can redistribute it and/or modify
+#    ProtectLinux ( is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
